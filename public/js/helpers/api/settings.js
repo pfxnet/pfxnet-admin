117aa41c577f45e45bd8f0e9daf61420646b6c75
junicodes <junicodesdev@gmail.com>
2021-01-15 13:51:30 +0100

    fix user edit page

--- public/js/helpers/api/settings.js
+++ public/js/helpers/api/settings.js
@@ -1,19 +1,18 @@
 
+const settingSearchInput = document.querySelector('#settingSearchInput');
+const copyRefLink = document.querySelector('#copyRefLink');
+
 //Show Query
-const settingShowInput = document.querySelector('#settingShowIn');
-const settingFirstnameEdit = document.querySelector("#settingFirstnameEdit")
-const settingLastnameEdit = document.querySelector("#settingLastnameEdit")
-const settingPhoneEdit = document.querySelector("#settingPhoneEdit")
-const settingDOBEdit = document.querySelector("#settingDOBEdit")
-const settingGenderEdit = document.querySelector("#settingGenderEdit")
-const settingAddressEdit = document.querySelector("#settingAddressEdit")
-const settingCountryEdit = document.querySelector("#settingCountryEdit")
-const settingStateEdit = document.querySelector("#settingStateEdit")
-const settingCityEdit = document.querySelector("#settingCityEdit")
-const settingUserIDEdit = document.querySelector("#settingUserIDEdit")
+const settingNameShow = document.querySelector("#settingNameShow")
+const settingEmailShow = document.querySelector("#settingEmailShow")
+const settingPhoneShow= document.querySelector("#settingPhoneShow")
+const settingJoined = document.querySelector("#settingJoined")
+const settingReferralTitle = document.querySelector("#settingReferralTitle")
+const settingReferralLink = document.querySelector("#settingReferralLink")
+
+console.log(copyRefLink)
 
 //Edit Query 
-const settingSearchInput = document.querySelector('#settingSearchInput');
 const settingFirstnameEdit = document.querySelector("#settingFirstnameEdit")
 const settingLastnameEdit = document.querySelector("#settingLastnameEdit")
 const settingPhoneEdit = document.querySelector("#settingPhoneEdit")
@@ -35,10 +34,11 @@ const findToEditUser = (e) => {
     http.get(`api/searchUserProfile?search=${userInfo}`)
     .then((data) => {
         console.log(data)
-    
         if(data) {
-            const {firstname, lastname, phone, gender, address, country, state, city} = data;
-            
+            //Show the User
+            inputForUpdate(data)
+            displayUserData(data)
+            copyRefLink.style.display = 'block';
             
         }
 
@@ -49,3 +49,27 @@ const findToEditUser = (e) => {
 }
 
 settingSearchInput.addEventListener('keyup', (e) => findToEditUser(e))
+
+
+
+const copyReferralLink = (e) => {
+    e.preventDefault()
+    var copyText = document.getElementById("settingReferralLink");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    // alert("Copied the text: " + copyText.value);
+    copyRefLink.textContent = 'Copied';
+    copyRefLink.style.background = 'black';
+  }
+
+//   function myFunction(e) {
+//       e.preventDefault()
+//     var copyText = document.getElementById("myInput");
+//     copyText.select();
+//     copyText.setSelectionRange(0, 99999)
+//     document.execCommand("copy");
+//     alert("Copied the text: " + copyText.value);
+//   }
+
+copyRefLink.addEventListener('click', (e) => copyReferralLink(e))
---
+++ public/js/helpers/domInsert/showUser.js
@@ -0,0 +1,26 @@
+
+
+const displayUserData = (data) => {
+    console.log(data)
+    console.log(settingReferralLink.value, data, data.link)
+
+    settingNameShow.textContent = data != 'Empty Search..' ? `${data.firstname} ${data.lastname}` : '----';
+    settingEmailShow.textContent =  data != 'Empty Search..' ? data.mail : '----';
+    settingPhoneShow.textContent = data != 'Empty Search..' ? data.phone : '----';
+    settingJoined.textContent = data != 'Empty Search..' ? data.createdAt : '----';
+    settingReferralLink.value = data != 'Empty Search..' ? data.link : '';
+}
+
+const inputForUpdate = (data) => {
+    console.log(data)
+    settingFirstnameEdit.value = data.firstname ? data.firstname : '';
+    settingLastnameEdit.value = data.lastname ? data.lastname : '';
+    settingPhoneEdit.value = data.phone ? data.phone : '';
+    settingDOBEdit.value = data.dob ? data.dob : '';
+    settingGenderEdit.value = data.gender ? data.gender : '';
+    settingAddressEdit.value = data.address ? data.address : '';
+    settingCountryEdit.value = data.address ? data.address : '';
+    settingStateEdit.value = data.state ? data.state : '';
+    settingCityEdit.value = data.city ? data.city : '';
+    settingUserIDEdit.value = data.userId ? data.userId : '';
+}
